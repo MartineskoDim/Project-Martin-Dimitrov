2d2644b72c701d8659ca742aeab2850ef8255ad7
MEGA <MEGA@gmail.com>
2025-02-17 12:18:40 +0200

    Add n++ feature to Print

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,14 +1,18 @@
-﻿namespace Test
+﻿using System.Runtime.CompilerServices;
+
+namespace Test
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int n = 1;
             Console.WriteLine(Console.ReadLine());
+            Print(n);
         }
-        public static void Print()
+        public static void Print(int n)
         {
-            Console.WriteLine(Console.Read());
+            Console.WriteLine($"{n++}{Console.Read()}");
         }
     }
 }
