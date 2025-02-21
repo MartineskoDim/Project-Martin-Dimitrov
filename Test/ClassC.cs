9f91c29856901eb678b8332b045f7592ce62b308
MEGA <MEGA@gmail.com>
2025-02-21 11:19:04 +0200

    Matematicheski fomuli

---
+++ Test/ClassC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class ClassC
+    {
+        public double Sqrt(double a)
+        {
+            return Math.Sqrt(a);
+        }
+    }
+}
